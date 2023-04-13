62f1b0fc11117408cb0483af93ed755fba781969
newmancu <newmancu@gmail.com>
2023-04-13 23:54:04 +0300

    fix. entrypoint.sh

--- configs/entrypoint.sh
+++ configs/entrypoint.sh
@@ -13,15 +13,20 @@ done
 echo "REDIS CONNECTED"
 
 cd ./parcel
-python3 manage.py makemigrations
-python3 manage.py makemigrations parcel_api
-python3 manage.py migrate
-python3 manage.py base_configuration
 if [[ "$CELERY" -eq 0 ]] 
 then
+  python3 manage.py makemigrations
+  python3 manage.py makemigrations parcel_api
+  python3 manage.py migrate
+  python3 manage.py base_configuration
   # python3 manage.py collectstatic --noinput
   python3 manage.py runserver 0.0.0.0:8000
 else
+  echo "CONNECTING TO parcel_web"
+  while ! nc -z parcel_web 8000; do
+    sleep .2s
+  done
+  echo "parcel_web CONNECTED"
   if [[ "$CELERY_BEAT" -eq 0 ]]
   then 
     python3 -m celery -A parcel worker -l info
